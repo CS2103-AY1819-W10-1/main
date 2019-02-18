1bb014a6fce36b659c279f1f22d71f56414af40c
Jonathan Cheng <36514601+epicfailname@users.noreply.github.com>
2019-02-18 23:45:41 +0800

    Added and updated tests for ResultDisplay
    Tests to check style for successful and error feedback

--- src/test/java/seedu/address/ui/ResultDisplayTest.java
+++ src/test/java/seedu/address/ui/ResultDisplayTest.java
@@ -1,6 +1,9 @@
 package seedu.address.ui;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +15,9 @@ public class ResultDisplayTest extends GuiUnitTest {
     private ResultDisplay resultDisplay;
     private ResultDisplayHandle resultDisplayHandle;
 
+    private ArrayList<String> defaultStyleOfResultDisplay;
+    private ArrayList<String> errorStyleOfResultDisplay;
+    
     @Before
     public void setUp() {
         resultDisplay = new ResultDisplay();
@@ -19,17 +25,81 @@ public class ResultDisplayTest extends GuiUnitTest {
 
         resultDisplayHandle = new ResultDisplayHandle(getChildNode(resultDisplay.getRoot(),
                 ResultDisplayHandle.RESULT_DISPLAY_ID));
+
+        defaultStyleOfResultDisplay = new ArrayList<>(resultDisplayHandle.getStyleClass());
+
+        errorStyleOfResultDisplay = new ArrayList<>(defaultStyleOfResultDisplay);
+        errorStyleOfResultDisplay.add(CommandBox.ERROR_STYLE_CLASS);
     }
 
     @Test
-    public void display() {
+    public void defaultDisplay_emptyDisplay() {
         // default result text
         guiRobot.pauseForHuman();
         assertEquals("", resultDisplayHandle.getText());
-
-        // new result received
-        guiRobot.interact(() -> resultDisplay.setFeedbackToUser("Dummy feedback to user"));
+    }
+    
+    @Test
+    public void emptyFeedback_emptyDisplay() {
+        guiRobot.interact(() -> resultDisplay.setFeedbackSuccessToUser(""));
+        guiRobot.pauseForHuman();
+        assertEquals("", resultDisplayHandle.getText());
+        assertEquals(defaultStyleOfResultDisplay, resultDisplayHandle.getStyleClass());
+    }
+    @Test
+    public void successfulFeedback() {
+        guiRobot.interact(() -> resultDisplay.setFeedbackSuccessToUser("Dummy feedback to user"));
         guiRobot.pauseForHuman();
         assertEquals("Dummy feedback to user", resultDisplayHandle.getText());
+        assertStyleForSuccessfulFeedback(resultDisplayHandle);
+    }
+    
+    @Test
+    public void errorFeedback() {
+        guiRobot.interact(() -> resultDisplay.setFeedbackErrorToUser("Dummy feedback to user"));
+        guiRobot.pauseForHuman();
+        assertEquals("Dummy feedback to user", resultDisplayHandle.getText());
+        assertStyleForErrorFeedback(resultDisplayHandle);
+    }
+    
+    @Test
+    public void consecutiveFeedBack() {
+        // new error result received
+        guiRobot.interact(() -> resultDisplay.setFeedbackErrorToUser("Dummy feedback to user"));
+        guiRobot.pauseForHuman();
+        assertEquals("Dummy feedback to user", resultDisplayHandle.getText());
+        assertStyleForErrorFeedback(resultDisplayHandle);
+        
+        // new error result received
+        guiRobot.interact(() -> resultDisplay.setFeedbackErrorToUser("Dummy feedback to user"));
+        guiRobot.pauseForHuman();
+        assertEquals("Dummy feedback to user", resultDisplayHandle.getText());
+        assertStyleForErrorFeedback(resultDisplayHandle);
+        
+        // new success result received
+        guiRobot.interact(() -> resultDisplay.setFeedbackSuccessToUser("Dummy feedback to user"));
+        guiRobot.pauseForHuman();
+        assertEquals("Dummy feedback to user", resultDisplayHandle.getText());
+        assertStyleForSuccessfulFeedback(resultDisplayHandle);
+
+        // new success result received
+        guiRobot.interact(() -> resultDisplay.setFeedbackSuccessToUser("Dummy feedback to user"));
+        guiRobot.pauseForHuman();
+        assertEquals("Dummy feedback to user", resultDisplayHandle.getText());
+        assertStyleForSuccessfulFeedback(resultDisplayHandle);
+
+        // new error result received
+        guiRobot.interact(() -> resultDisplay.setFeedbackErrorToUser("Dummy feedback to user"));
+        guiRobot.pauseForHuman();
+        assertEquals("Dummy feedback to user", resultDisplayHandle.getText());
+        assertStyleForErrorFeedback(resultDisplayHandle);
+    }
+    
+    private void assertStyleForSuccessfulFeedback(ResultDisplayHandle resultDisplayHandle) {
+        assertEquals(defaultStyleOfResultDisplay, resultDisplayHandle.getStyleClass());
+    }
+
+    private void assertStyleForErrorFeedback(ResultDisplayHandle resultDisplayHandle) {
+        assertEquals(errorStyleOfResultDisplay, resultDisplayHandle.getStyleClass());
     }
 }
