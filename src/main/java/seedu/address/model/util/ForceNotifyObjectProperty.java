9d70c07d36d32f100983c2e011a61cb05e477b93
rlrh <rlrh1996@gmail.com>
2019-04-10 09:42:24 +0800

    Change CommandResultProperty, ExceptionProperty to force notify listeners

---
+++ src/main/java/seedu/address/model/util/ForceNotifyObjectProperty.java
@@ -0,0 +1,10 @@
+package seedu.address.model.util;
+
+import javafx.beans.property.SimpleObjectProperty;
+
+public class ForceNotifyObjectProperty<T> extends SimpleObjectProperty<T> {
+    @Override
+    public void fireValueChangedEvent() {
+        super.fireValueChangedEvent();
+    }
+}
